a76e05ef6c2ace8d2c2063ca124e5f53c6102705
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-05-27 04:51:09 +0100

    Move Paddle

--- script.js
+++ script.js
@@ -88,14 +88,66 @@ function drawBricks() {
   });
 }
 
+// Move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // Wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
 // Draw everything
 function draw() {
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // To see the effect comment it this line and play game
+
   drawBall();
   drawPaddle();
   drawScore();
   drawBricks();
 }
-draw();
+
+// Update canvas drawing and animation
+function update() {
+  movePaddle();
+  // Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+// Keyup event
+function keyUP(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    paddle.dx = 0;
+  }
+}
+
+// Keybord event handler
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUP);
 
 // Rules and close event handler
 rulesBtn.addEventListener("click", () => {
